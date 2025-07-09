14ea62d7c37e209d0c3bba09611404b4c108be1a
Leonardo Torres <leonardootgs@gmail.com>
2025-07-09 20:57:13 -0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 let startBtn = document.getElementById('start-btn');
-let result = document.getElementById('result')
+let textResult = document.getElementById('result')
+let inputCharacter = document.getElementById('char');
+let inputCount = document.getElementById('length');
 
-startBtn.addEventListener('click', alerta);
+startBtn.addEventListener('click', printResult);
 
-function alerta() {
-    result.textContent = 'voce clicou';
+function printResult() {
+  
+  const character = inputCharacter.value;
+  const count = inputCount.value;
+
+  if (startBtn.textContent == 'Start') {
+    for (let i = 1; i <= count; i = i + 1) {
+      setTimeout(() => {
+        textResult.textContent += character.repeat(i);
+        textResult.textContent += '\n';
+
+        if(i == count){
+          triggerAnimation();
+        }
+      }, i * 150);
+    }
+    startBtn.textContent = 'Reset'
+  }
+  else {
+    textResult.textContent = ''
+    startBtn.textContent = 'Start';
+  }
 }
 
+function triggerAnimation() {
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 }
+  });
+}
